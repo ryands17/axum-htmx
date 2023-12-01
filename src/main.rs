7e41c7b795fe25b680e59133848333c8ee38cc95
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-12-01 19:08:24 +0100

    upgrade to axum 0.7

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ mod errors;
 mod filters;
 mod todos;
 
-use std::net::SocketAddr;
-
 use anyhow::Result;
 use askama::Template;
 use axum::{response::IntoResponse, routing::get, Router};
+use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 #[derive(Template)]
@@ -32,13 +31,12 @@ async fn main() -> Result<()> {
     .init();
 
   let app = router();
-  let addr = SocketAddr::from(([0, 0, 0, 0], 3001));
+  let listener = TcpListener::bind("127.0.0.1:3001").await.unwrap();
+  let addr = listener.local_addr().unwrap().to_string();
 
   tracing::info!("App running on http://{}", addr);
 
-  axum::Server::bind(&addr)
-    .serve(app.into_make_service())
-    .await?;
+  axum::serve(listener, app.into_make_service()).await?;
 
   Ok(())
 }
